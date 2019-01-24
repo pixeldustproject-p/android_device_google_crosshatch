d540d780bcdb984829f65b93fa97d37246d6084f
spezi77 <spezi7713@gmx.net>
2019-01-24 23:14:06 +0100

    blueline: Change vendor path to match with ours.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -253,5 +253,3 @@ endif
 
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/b1c1-setup.sh
-
--include vendor/google_devices/crosshatch/proprietary/BoardConfigVendor.mk
